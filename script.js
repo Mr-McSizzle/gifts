a4715f0f22b28822bfbdb935eff554c85b228543
Mr-McSizzle <76867623+Mr-McSizzle@users.noreply.github.com>
2025-03-16 20:56:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // script.js
 document.getElementById('giftCard').addEventListener('click', function() {
-    var message = document.getElementById('message');
-    if (message.style.display === 'none' || message.style.display === '') {
-        message.style.display = 'block';
-    } else {
-        message.style.display = 'none';
-    }
+    var card = document.getElementById('giftCard');
+    card.classList.toggle('open');
 });
